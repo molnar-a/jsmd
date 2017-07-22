12fe85686f6ee3af06d3aed000eee4868769b703
Midnightas <pan.totoro@gmail.com>
2017-07-22 18:22:32 +0300

    Create jsmd.js

---
+++ jsmd.js
@@ -0,0 +1,26 @@
+(() => {
+  a = [
+    [/^(#{1,6})(.*)$/gm, (m,h,p) => {
+      hs = h.length;
+      return "<h" + hs + ">" + p + "</h" + hs + ">"
+    }],
+    [/\*\*(.*)\*\*/gm, "<b>$1</b>"],
+    [/__(.*)__/gm, "<b>$1</b>"],
+    [/_(.*)_/gm, "<i>$1</i>"],
+    [/`(.*)`/gm, "<pre>$1</pre>"],
+    [/\[(.*)\]\((.*)\)/gm, "<a href='$2'>$1</a>"],
+    [/^---\s$/gm, "<hr/>"],
+    [/(\n\*.*)+/gm, m => {
+      s = "";
+      m.substring(1).split("\n").forEach(z => s += "<li>" + z.substring(2) + "</li>");
+      return "<ul>" + s + "</ul>"
+    }],
+    [/(\n\d\..*)+/gm, m => {
+      s="";
+      m.substring(1).split("\n").forEach(z => s += "<li>" + z.substring(2) + "</li>");
+      return "<ol>" + s + "</ol>"
+    }],
+    [/\n\n/gm, "</p><p>"]
+  ];
+  window.jsmd = s => "<p>" + [a.forEach(b => s = s.replace(b[0], b[1])), s][1] + "</p>"
+})();
